e9b0ff31a5090f9aeef5649903a2e623cb812ff3
Antonio Epifani <antonio.epifani@keyless.io>
2026-06-25 10:44:07 +0200

    feat(device selection): can select device to build on

--- configureProject.sh
+++ configureProject.sh
@@ -26,6 +26,50 @@ selectScheme() {
 	echo "$1" > $XCLI_FOLDER/selectedScheme 
 }
 
+selectDeviceByName() {
+	if [[ -z "$1" ]]; then 
+		fail "Expected to receive the name of the device, but received nothing"
+	fi
+
+	echo "Name: $1" > $XCLI_FOLDER/selectedDevice 
+}
+
+selectDeviceByIdentifier() {
+	if [[ -z "$1" ]]; then 
+		fail "Expected to receive the identifier of the device, but received nothing"
+	fi
+
+	echo "Identifier: $1" > $XCLI_FOLDER/selectedDevice 
+}
+
+openDeviceSelector() {
+	xcrun xctrace list devices | awk '
+	function printNameAndIdentifier(type) {
+			name = ""
+			if (match($0, /[^(]*\(/)) {
+				name = substr($0, RSTART, RLENGTH-2)
+			}
+
+			identifier = substr($NF, 2, length($NF)-2)
+
+			printf("\"%s\" %s (%s)\n", name, identifier, type)
+	}
+
+	/== Devices/,/== Simulator/ {
+		if(!/(^==|^$)/) {
+			printNameAndIdentifier("Device")
+		}
+	}
+
+	/== Simulator/ {found=1} found {
+		if(!/(^==|^$)/) {
+			printNameAndIdentifier("Simulator")
+		}
+	}
+	' | fzf | \
+		awk '{ printf("Identifier: %s %s\n", substr($(NF-1), 1, length($(NF-1))), $NF) }'  > $XCLI_FOLDER/selectedDevice
+}
+
 openProjectSelector() {
 	echo $(cat $XCLI_FOLDER/projects | fzf) > $XCLI_FOLDER/selectedProject
 }
